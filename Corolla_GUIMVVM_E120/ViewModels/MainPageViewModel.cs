613da8a4f746df6c1bf0364b46e573024c8de3e3
Swift <bryam1398@gmail.com>
2021-08-17 16:18:45 -0500

    Eliminacion de definiciones no utilizadas

--- Corolla_GUIMVVM_E120/ViewModels/MainPageViewModel.cs
+++ Corolla_GUIMVVM_E120/ViewModels/MainPageViewModel.cs
@@ -1,26 +1,15 @@
-﻿using Corolla_GUIMVVM_E120.Services.DialogService;
-using Corolla_GUIMVVM_E120.Services.LoaderService;
-using Corolla_GUIMVVM_E120.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Corolla_GUIMVVM_E120.ViewModels.Base;
 using System.Threading.Tasks;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
 
 namespace Corolla_GUIMVVM_E120.ViewModels
 {
     public class MainPageViewModel : ViewModelPageBase
     {
-        IDialogService _dialogService;
-        ILoaderService _loaderService;
 
-        public MainPageViewModel(IDialogService dialogService, ILoaderService loaderService)
+        public MainPageViewModel()
         {
-            _dialogService = dialogService;
-            _loaderService = loaderService;
-            
+
         }
 
         public override Task OnNavigatedFrom(NavigationEventArgs args)
